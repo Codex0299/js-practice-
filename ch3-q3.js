cff310cd8f6fdd87e7c7e67ea5b0f60148019bdc
Codex0299 <130696733+Codex0299@users.noreply.github.com>
2024-07-06 22:35:53 +0530

    Add files via upload

---
+++ ch3-q3.js
@@ -0,0 +1,5 @@
+//Write a for loop to print the multiplication table of a given number up to 10.
+let num = 5; // Example number
+for (let i = 1; i <= 10; i++) {
+    console.log(`${num} * ${i} = ${num * i}`);
+}
---
+++ ch3-q4.js
@@ -0,0 +1,7 @@
+//Write a for loop to calculate the sum of all elements in an array.
+let arr = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+console.log("Sum:", sum);
